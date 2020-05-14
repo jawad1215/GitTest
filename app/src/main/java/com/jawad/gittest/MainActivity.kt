f90e055800f4141ac77b35f965337fda759f239b
jawad1215 <jawadyousafkhan@gmail.com>
2020-05-14 12:34:29 +0500

    Initial Commit

---
+++ app/src/main/java/com/jawad/gittest/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.jawad.gittest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var button:Button
+    var textView:TextView?=null
+    var increment=0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button=findViewById(R.id.button)
+        textView=findViewById(R.id.textView)
+        button.setOnClickListener { inCrement() }
+    }
+
+    private fun inCrement() {
+        increment += 2
+        textView?.setText(increment.toString())
+
+    }
+
+
+}
